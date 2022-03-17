6a620eacc561398846e9b841e5b07111678bda3a
postor <postor@gmail.com>
2022-03-17 18:54:26 +0800

    basic

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export { MenuAim } from './MenuAim'
 export { Menu } from './Menu'
 export { SubMenuPanel } from './SubMenuPanel'
+
+import { menuCtx } from './ctx'
+
+export const useMenu = menuCtx.useCtx
